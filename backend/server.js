7f4039152f6d48c638b02bdb152c61ac175d2fa2
Alvindra Aditya <alvindra14@gmail.com>
2023-02-12 02:25:50 +0700

    pengaplikasian nodemon

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ const app = express();
 
 app.get("/restoran",(req, res) => {
     console.log("semua data restoran")
-    
+
 });
 //http://localhost:2023/2024/restoran
 
 const port = process.env.port || 2024;
 // NOTE : jika file env yang menyimpan alamat port tidak di temukan, maka akan membaca port sebagai 2024
 app.listen(port, () => {
-            console.log('terhubung ke server port ${port}');
+            console.log(`terhubung ke server port ${port}`);
     }
 );
