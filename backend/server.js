00012ccb5edcb4b2ffe35fcb42ea98f1a76d8b05
Alvindra Aditya <alvindra14@gmail.com>
2023-02-12 02:21:37 +0700

    get restoran

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ require ("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.get("/restoran",(req, res) => {
+    console.log("semua data restoran")
+});
+//http://localhost:2023/2024/restoran
+
 const port = process.env.port || 2024;
 // NOTE : jika file env yang menyimpan alamat port tidak di temukan, maka akan membaca port sebagai 2024
 app.listen(port, () => {
